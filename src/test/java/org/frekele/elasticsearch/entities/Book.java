2dee5caa2bce1680a391978d01b840265cbf2f98
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-16 06:02:50 -0300

    book entity mock for test.

--- src/test/java/org/frekele/elasticsearch/entities/Book.java
+++ src/test/java/org/frekele/elasticsearch/entities/Book.java
@@ -1,8 +1,48 @@
 package org.frekele.elasticsearch.entities;
 
+import org.frekele.elasticsearch.annotations.Field;
+import org.frekele.elasticsearch.annotations.Fields;
+import org.frekele.elasticsearch.annotations.Mapping;
+import org.frekele.elasticsearch.enums.FieldType;
+
+@Mapping
 public class Book {
 
-    private String id;
+    @Field(type = FieldType.LONG)
+    private Long id;
 
+    @Field(type = FieldType.TEXT)
     private String name;
+
+    @Fields(type = FieldType.TEXT, fields = {
+        @Field(type = FieldType.KEYWORD)}
+    )
+    private String description;
+
+    public Book() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
