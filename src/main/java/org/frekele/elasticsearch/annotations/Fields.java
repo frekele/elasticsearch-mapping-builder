dbd7b7b0e5f7da5d4334e5f79891cd409fd49ef1
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-16 06:02:23 -0300

    create Fields

---
+++ src/main/java/org/frekele/elasticsearch/annotations/Fields.java
@@ -0,0 +1,17 @@
+package org.frekele.elasticsearch.annotations;
+
+import org.frekele.elasticsearch.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Fields {
+
+    FieldType type();
+
+    Field[] fields();
+}
