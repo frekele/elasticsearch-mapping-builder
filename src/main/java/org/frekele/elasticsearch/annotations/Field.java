f4508f079063466641cd953681102d4243004307
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-16 06:02:13 -0300

    add type

--- src/main/java/org/frekele/elasticsearch/annotations/Field.java
+++ src/main/java/org/frekele/elasticsearch/annotations/Field.java
@@ -1,5 +1,7 @@
 package org.frekele.elasticsearch.annotations;
 
+import org.frekele.elasticsearch.enums.FieldType;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,4 +11,5 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Field {
 
+    FieldType type();
 }
