27861da7def295add00ab68792b1f5e3d98d6dc8
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-16 03:44:08 -0300

    created MappingBuilderTest

---
+++ src/test/java/org/frekele/elasticsearch/MappingBuilderTest.java
@@ -0,0 +1,18 @@
+package org.frekele.elasticsearch;
+
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import static org.testng.Assert.*;
+
+public class MappingBuilderTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+    }
+
+    @AfterMethod
+    public void tearDown() throws Exception {
+    }
+
+}
