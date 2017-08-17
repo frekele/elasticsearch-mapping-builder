faa38fdb8f52a69f37a1c347adf5c5d364d6e250
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 06:44:01 -0300

    ElasticGeoShapeField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticGeoShapeField.java
@@ -0,0 +1,40 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The geo_shape datatype facilitates the indexing of and searching with arbitrary geo shapes such as rectangles and polygons.
+ * It should be used when either the data being indexed or the queries being executed contain shapes other than just points.
+ *
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/geo-point.html">elasticsearch geo shape field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticGeoShapeField {
+
+    FieldType type = FieldType.GEO_SHAPE;
+
+    String suffixName() default "geoShape";
+
+    String tree() default "";
+
+    String precision() default "";
+
+    String treeLevels() default "";
+
+    String strategy() default "";
+
+    float distanceErrorPct() default 0.025f;
+
+    String orientation() default "";
+
+    boolean pointsOnly() default false;
+
+}
+
+
