517b14111d68df5402b3d27c47c1a5a3073a8467
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 05:26:45 -0300

    .

--- src/test/java/org/frekele/elasticsearch/mapping/entities/NoBook.java
+++ src/test/java/org/frekele/elasticsearch/mapping/entities/NoBook.java
@@ -11,27 +11,4 @@ public class NoBook {
     public NoBook() {
     }
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
