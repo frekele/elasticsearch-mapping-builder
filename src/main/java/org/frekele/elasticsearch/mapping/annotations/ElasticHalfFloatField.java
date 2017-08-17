bdfc45631a4adad5170ea76acc305116c29282ab
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 03:33:22 -0300

    ElasticHalfFloatField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticHalfFloatField.java
@@ -0,0 +1,35 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/number.html">elasticsearch half float number field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticHalfFloatField {
+
+    FieldType type = FieldType.HALF_FLOAT;
+
+    String suffixName() default "halfFloat";
+
+    boolean coerce() default true;
+
+    float boost() default 1.0f;
+
+    boolean docValues() default false;
+
+    boolean ignoreMalformed() default false;
+
+    boolean index() default true;
+
+    String nullValue() default "";
+
+    boolean store() default false;
+
+}
