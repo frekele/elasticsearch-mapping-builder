f522af856881e766d88f5320e4736ca7be9d0aa3
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 07:38:19 -0300

    ElasticObjectField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticObjectField.java
@@ -0,0 +1,21 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * JSON documents are hierarchical in nature: the document may contain inner objects which, in turn, may contain inner objects themselves.
+ *
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/object.html">elasticsearch object field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticObjectField {
+
+    FieldType type = FieldType.OBJECT;
+
+}
