7c503778606b35f9acd11d79c349e049d1744b7a
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 07:48:08 -0300

    ElasticNestedField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticNestedField.java
@@ -0,0 +1,21 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The nested type is a specialised version of the object datatype that allows arrays of objects to be indexed and queried independently of each other..
+ *
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/nested.html">elasticsearch nested field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticNestedField {
+
+    FieldType type = FieldType.NESTED;
+
+}
