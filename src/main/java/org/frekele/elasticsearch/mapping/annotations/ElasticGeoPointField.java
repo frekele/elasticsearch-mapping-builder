ba9ea2fe292769df97b28b0b64ef6f914ecf9381
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 06:33:53 -0300

    ElasticGeoPointField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticGeoPointField.java
@@ -0,0 +1,29 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Fields of type geo_point accept latitude-longitude pairs, which can be used:
+ * - to find geo-points within a bounding box, within a certain distance of a central point, or within a polygon.
+ * - to aggregate documents by geographically or by distance from a central point.
+ * - to integrate distance into a document’s relevance score.
+ * - to sort documents by distance.
+ *
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/geo-point.html">elasticsearch geo point field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticGeoPointField {
+
+    FieldType type = FieldType.GEO_POINT;
+
+    String suffixName() default "geoPoint";
+
+    boolean ignoreMalformed() default false;
+
+}
