a6c23ca125fc71d3c5a8e605cbd2906b433257cc
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 03:29:48 -0300

    ElasticByteField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticByteField.java
@@ -0,0 +1,35 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/number.html">elasticsearch byte number field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticByteField {
+
+    FieldType type = FieldType.BYTE;
+
+    String suffixName() default "byte";
+
+    boolean coerce() default true;
+
+    float boost() default 1.0f;
+
+    boolean docValues() default false;
+
+    boolean ignoreMalformed() default false;
+
+    boolean index() default true;
+
+    String nullValue() default "";
+
+    boolean store() default false;
+
+}
