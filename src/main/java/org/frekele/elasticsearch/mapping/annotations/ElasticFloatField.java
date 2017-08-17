3e150cda4e53c80079deca03f72f52048cd16755
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 03:31:27 -0300

    ElasticFloatField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticFloatField.java
@@ -0,0 +1,35 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/number.html">elasticsearch float number field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticFloatField {
+
+    FieldType type = FieldType.FLOAT;
+
+    String suffixName() default "float";
+
+    boolean coerce() default true;
+
+    float boost() default 1.0f;
+
+    boolean docValues() default false;
+
+    boolean ignoreMalformed() default false;
+
+    boolean index() default true;
+
+    String nullValue() default "";
+
+    boolean store() default false;
+
+}
