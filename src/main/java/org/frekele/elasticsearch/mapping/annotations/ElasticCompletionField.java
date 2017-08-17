fb39ef301ea40351908537baa1d47e2de1c60049
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 07:02:21 -0300

    ElasticCompletionField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticCompletionField.java
@@ -0,0 +1,34 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * The completion suggester provides auto-complete/search-as-you-type functionality.
+ * This is a navigational feature to guide users to relevant results as they are typing, improving search precision.
+ * It is not meant for spell correction or did-you-mean functionality like the term or phrase suggesters.
+ *
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/search-suggesters-completion.html">elasticsearch completion field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticCompletionField {
+
+    FieldType type = FieldType.COMPLETION;
+
+    String suffixName() default "completion";
+
+    String analyzer() default "";
+
+    String searchAnalyzer() default "";
+
+    boolean preserveSeparators() default true;
+
+    boolean preservePositionIncrements() default true;
+
+    int maxInputLength() default 50;
+}
