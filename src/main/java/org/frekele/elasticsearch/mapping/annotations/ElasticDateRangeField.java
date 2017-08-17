9d12964ec2a09f632650949dc6eeae5c153af2b1
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 06:04:05 -0300

    ElasticDateRangeField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticDateRangeField.java
@@ -0,0 +1,39 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * A range of date values represented as unsigned 64-bit integer milliseconds elapsed since system epoch.
+ *
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/range.html">elasticsearch date range field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticDateRangeField {
+
+    FieldType type = FieldType.DATE_RANGE;
+
+    String suffixName() default "dateRange";
+
+    float boost() default 1.0f;
+
+    boolean docValues() default false;
+
+    String format() default "";
+
+    String locale() default "";
+
+    boolean ignoreMalformed() default false;
+
+    boolean index() default true;
+
+    String nullValue() default "";
+
+    boolean store() default false;
+
+}
