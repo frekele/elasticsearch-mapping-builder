731954b0bd139cdd02a607dadf7e4a9d5d4e6313
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 04:19:54 -0300

    refactoring, improvement OO.

--- src/main/java/org/frekele/elasticsearch/mapping/MappingBuilder.java
+++ src/main/java/org/frekele/elasticsearch/mapping/MappingBuilder.java
@@ -2,9 +2,18 @@ package org.frekele.elasticsearch.mapping;
 
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.frekele.elasticsearch.mapping.annotations.ElasticByteField;
 import org.frekele.elasticsearch.mapping.annotations.ElasticDocument;
+import org.frekele.elasticsearch.mapping.annotations.ElasticDoubleField;
+import org.frekele.elasticsearch.mapping.annotations.ElasticFloatField;
+import org.frekele.elasticsearch.mapping.annotations.ElasticHalfFloatField;
+import org.frekele.elasticsearch.mapping.annotations.ElasticIntegerField;
 import org.frekele.elasticsearch.mapping.annotations.ElasticKeywordField;
+import org.frekele.elasticsearch.mapping.annotations.ElasticLongField;
+import org.frekele.elasticsearch.mapping.annotations.ElasticScaledFloatField;
+import org.frekele.elasticsearch.mapping.annotations.ElasticShortField;
 import org.frekele.elasticsearch.mapping.annotations.ElasticTextField;
+import org.frekele.elasticsearch.mapping.annotations.values.ElasticFielddataFrequencyFilter;
 import org.frekele.elasticsearch.mapping.exceptions.InvalidDocumentClassException;
 
 import java.io.IOException;
@@ -42,15 +51,6 @@ public class MappingBuilder implements Serializable {
         }
     }
 
-    static boolean containElasticFieldAnnotation(Field field) {
-        List<Annotation> result = getElasticFieldAnnotations(field);
-        return (result != null && !result.isEmpty());
-    }
-
-    static boolean isElasticFieldAnnotation(Annotation annotation) {
-        return (annotation instanceof ElasticTextField || annotation instanceof ElasticKeywordField);
-    }
-
     static List<Annotation> getElasticFieldAnnotations(Field field) {
         List<Annotation> result = new ArrayList<>();
         Annotation[] annotations = field.getDeclaredAnnotations();
@@ -66,126 +66,209 @@ public class MappingBuilder implements Serializable {
         }
     }
 
-    void processElasticField(Annotation annotation, boolean subField) throws IOException {
+    static boolean containElasticFieldAnnotation(Field field) {
+        List<Annotation> result = getElasticFieldAnnotations(field);
+        return (result != null && !result.isEmpty());
+    }
+
+    static boolean isElasticFieldAnnotation(Annotation annotation) {
+        return (annotation instanceof ElasticTextField || annotation instanceof ElasticKeywordField
+            || annotation instanceof ElasticLongField || annotation instanceof ElasticIntegerField
+            || annotation instanceof ElasticShortField || annotation instanceof ElasticByteField
+            || annotation instanceof ElasticDoubleField || annotation instanceof ElasticFloatField
+            || annotation instanceof ElasticHalfFloatField || annotation instanceof ElasticScaledFloatField);
+    }
+
+    void processElasticAnnotationField(Annotation annotation, boolean subField) throws IOException {
+        //String datatypes
         if (annotation instanceof ElasticTextField) {
             this.processElasticField((ElasticTextField) annotation, subField);
-        }
-        if (annotation instanceof ElasticKeywordField) {
+        } else if (annotation instanceof ElasticKeywordField) {
             this.processElasticField((ElasticKeywordField) annotation, subField);
+        }
+        //Numeric datatypes
+//        else if (annotation instanceof ElasticLongField) {
+//            this.processElasticField((ElasticLongField) annotation, subField);
+//        } else if (annotation instanceof ElasticIntegerField) {
+//            this.processElasticField((ElasticIntegerField) annotation, subField);
+//        } else if (annotation instanceof ElasticShortField) {
+//            this.processElasticField((ElasticShortField) annotation, subField);
+//        } else if (annotation instanceof ElasticByteField) {
+//            this.processElasticField((ElasticByteField) annotation, subField);
+//        } else if (annotation instanceof ElasticDoubleField) {
+//            this.processElasticField((ElasticDoubleField) annotation, subField);
+//        } else if (annotation instanceof ElasticFloatField) {
+//            this.processElasticField((ElasticFloatField) annotation, subField);
+//        } else if (annotation instanceof ElasticHalfFloatField) {
+//            this.processElasticField((ElasticHalfFloatField) annotation, subField);
+//        } else if (annotation instanceof ElasticScaledFloatField) {
+//            this.processElasticField((ElasticScaledFloatField) annotation, subField);
+//        }
+    }
+
+    void startSuffixName(boolean subField, String suffixName) throws IOException {
+        if (subField) {
+            //Add suffixName to subField;
+            this.mapping.startObject(suffixName);
+        }
+    }
+
+    void closeSuffixName(boolean subField) throws IOException {
+        if (subField) {
+            //Add suffixName to subField;
+            this.mapping.endObject();
+        }
+    }
+
+    void analyzer(String analyzer) throws IOException {
+        if (!analyzer.isEmpty()) {
+            this.mapping.field("analyzer", analyzer);
+        }
+    }
+
+    void boost(float boost) throws IOException {
+        //default 1.0
+        if (boost != 1.0f) {
+            this.mapping.field("boost", boost);
+        }
+    }
+
+    void fielddata(boolean fielddata) throws IOException {
+        //Default false
+        if (fielddata) {
+            this.mapping.field("fielddata", fielddata);
+        }
+    }
+
+    void fielddataFrequencyFilter(ElasticFielddataFrequencyFilter fielddataFrequencyFilter) throws IOException {
+        if (fielddataFrequencyFilter.enabled()) {
+            this.mapping.startObject("fielddata_frequency_filter");
+            this.mapping.field("min", fielddataFrequencyFilter.min());
+            this.mapping.field("max", fielddataFrequencyFilter.max());
+            this.mapping.field("min_segment_size", fielddataFrequencyFilter.minSegmentSize());
+            //fielddata_frequency_filter
+            this.mapping.endObject();
+        }
+    }
+
+    void index(boolean index) throws IOException {
+        //Default true
+        if (!index) {
+            this.mapping.field("index", index);
+        }
+    }
+
+    void indexOptions(String indexOptions) throws IOException {
+        if (!indexOptions.isEmpty()) {
+            this.mapping.field("index_options", indexOptions);
+        }
+    }
+
+    void norms(boolean norms) throws IOException {
+        //Default true
+        if (!norms) {
+            this.mapping.field("norms", norms);
+        }
+    }
+
+    void positionIncrementGap(int positionIncrementGap) throws IOException {
+        //default 100
+        if (positionIncrementGap != 100) {
+            this.mapping.field("positionIncrementGap", positionIncrementGap);
+        }
+    }
+
+    void store(boolean store) throws IOException {
+        //Default false
+        if (store) {
+            this.mapping.field("store", store);
+        }
+    }
+
+    void searchAnalyzer(String searchAnalyzer) throws IOException {
+        if (!searchAnalyzer.isEmpty()) {
+            this.mapping.field("search_analyzer", searchAnalyzer);
+        }
+    }
+
+    void searchQuoteAnalyzer(String searchQuoteAnalyzer) throws IOException {
+        if (!searchQuoteAnalyzer.isEmpty()) {
+            this.mapping.field("search_quote_analyzer", searchQuoteAnalyzer);
+        }
+    }
+
+    void similarity(String similarity) throws IOException {
+        if (!similarity.isEmpty()) {
+            this.mapping.field("similarity", similarity);
+        }
+    }
+
+    void termVector(String termVector) throws IOException {
+        if (!termVector.isEmpty()) {
+            this.mapping.field("term_vector", termVector);
+        }
+    }
+
+    void docValues(boolean docValues) throws IOException {
+        //Default false
+        if (docValues) {
+            this.mapping.field("doc_values", docValues);
+        }
+    }
+
+    void ignoreAbove(int ignoreAbove) throws IOException {
+        //default 256
+        if (ignoreAbove != 256) {
+            this.mapping.field("ignore_above", ignoreAbove);
+        }
+    }
+
+    void nullValue(String nullValue) throws IOException {
+        if (!nullValue.isEmpty()) {
+            this.mapping.field("null_value", nullValue);
+        }
+    }
+
+    void normalizer(String normalizer) throws IOException {
+        if (!normalizer.isEmpty()) {
+            this.mapping.field("normalizer", normalizer);
         }
     }
 
     void processElasticField(ElasticTextField elasticField, boolean subField) throws IOException {
-        if (subField) {
-            //Add suffixName to subField;
-            this.mapping.startObject(elasticField.suffixName());
-        }
+        this.startSuffixName(subField, elasticField.suffixName());
         this.mapping.field("type", ElasticTextField.type.getName());
-        if (!elasticField.analyzer().isEmpty()) {
-            this.mapping.field("analyzer", elasticField.analyzer());
-        }
-        //default 100
-        if (elasticField.boost() != 1.0f) {
-            this.mapping.field("boost", elasticField.boost());
-        }
-        //Default false
-        if (elasticField.fielddata()) {
-            this.mapping.field("fielddata", elasticField.fielddata());
-        }
-        if (elasticField.fielddataFrequencyFilter().enabled()) {
-            this.mapping.startObject("fielddata_frequency_filter");
-            this.mapping.field("min", elasticField.fielddataFrequencyFilter().min());
-            this.mapping.field("max", elasticField.fielddataFrequencyFilter().max());
-            this.mapping.field("min_segment_size", elasticField.fielddataFrequencyFilter().minSegmentSize());
-            //fielddata_frequency_filter
-            this.mapping.endObject();
-        }
-        //Default true
-        if (!elasticField.index()) {
-            this.mapping.field("index", elasticField.index());
-        }
-        if (!elasticField.indexOptions().isEmpty()) {
-            this.mapping.field("index_options", elasticField.indexOptions());
-        }
-        //Default true
-        if (!elasticField.norms()) {
-            this.mapping.field("norms", elasticField.norms());
-        }
-        //default 100
-        if (elasticField.positionIncrementGap() != 100) {
-            this.mapping.field("positionIncrementGap", elasticField.positionIncrementGap());
-        }
-        //Default false
-        if (elasticField.store()) {
-            this.mapping.field("store", elasticField.store());
-        }
-        if (!elasticField.searchAnalyzer().isEmpty()) {
-            this.mapping.field("search_analyzer", elasticField.searchAnalyzer());
-        }
-        if (!elasticField.searchQuoteAnalyzer().isEmpty()) {
-            this.mapping.field("search_quote_analyzer", elasticField.searchQuoteAnalyzer());
-        }
-        if (!elasticField.similarity().isEmpty()) {
-            this.mapping.field("similarity", elasticField.similarity());
-        }
-        if (!elasticField.termVector().isEmpty()) {
-            this.mapping.field("term_vector", elasticField.termVector());
-        }
-        if (subField) {
-            //Close suffixName to subField;
-            this.mapping.endObject();
-        }
+        this.analyzer(elasticField.analyzer());
+        this.boost(elasticField.boost());
+        this.fielddata(elasticField.fielddata());
+        this.fielddataFrequencyFilter(elasticField.fielddataFrequencyFilter());
+        this.index(elasticField.index());
+        this.indexOptions(elasticField.indexOptions());
+        this.norms(elasticField.norms());
+        this.positionIncrementGap(elasticField.positionIncrementGap());
+        this.store(elasticField.store());
+        this.searchAnalyzer(elasticField.searchAnalyzer());
+        this.searchQuoteAnalyzer(elasticField.searchQuoteAnalyzer());
+        this.similarity(elasticField.similarity());
+        this.termVector(elasticField.termVector());
+        this.closeSuffixName(subField);
     }
 
     void processElasticField(ElasticKeywordField elasticField, boolean subField) throws IOException {
-        if (subField) {
-            //Add suffixName to subField;
-            this.mapping.startObject(elasticField.suffixName());
-        }
+        this.startSuffixName(subField, elasticField.suffixName());
         this.mapping.field("type", ElasticKeywordField.type.getName());
-        if (!elasticField.analyzer().isEmpty()) {
-            this.mapping.field("analyzer", elasticField.analyzer());
-        }
-        //default 100
-        if (elasticField.boost() != 1.0f) {
-            this.mapping.field("boost", elasticField.boost());
-        }
-        //Default false
-        if (elasticField.docValues()) {
-            this.mapping.field("doc_values", elasticField.docValues());
-        }
-        //default 256
-        if (elasticField.ignoreAbove() != 256) {
-            this.mapping.field("ignore_above", elasticField.ignoreAbove());
-        }
-        //Default true
-        if (!elasticField.index()) {
-            this.mapping.field("index", elasticField.index());
-        }
-        if (!elasticField.indexOptions().isEmpty()) {
-            this.mapping.field("index_options", elasticField.indexOptions());
-        }
-        //Default true
-        if (!elasticField.norms()) {
-            this.mapping.field("norms", elasticField.norms());
-        }
-        if (!elasticField.nullValue().isEmpty()) {
-            this.mapping.field("null_value", elasticField.nullValue());
-        }
-        //Default false
-        if (elasticField.store()) {
-            this.mapping.field("store", elasticField.store());
-        }
-        if (!elasticField.similarity().isEmpty()) {
-            this.mapping.field("similarity", elasticField.similarity());
-        }
-        if (!elasticField.normalizer().isEmpty()) {
-            this.mapping.field("normalizer", elasticField.normalizer());
-        }
-        if (subField) {
-            //Close suffixName to subField;
-            this.mapping.endObject();
-        }
+        this.analyzer(elasticField.analyzer());
+        this.boost(elasticField.boost());
+        this.docValues(elasticField.docValues());
+        this.ignoreAbove(elasticField.ignoreAbove());
+        this.index(elasticField.index());
+        this.indexOptions(elasticField.indexOptions());
+        this.norms(elasticField.norms());
+        this.nullValue(elasticField.nullValue());
+        this.store(elasticField.store());
+        this.similarity(elasticField.similarity());
+        this.normalizer(elasticField.normalizer());
+        this.closeSuffixName(subField);
     }
 
     public XContentBuilder source() throws IOException {
@@ -222,14 +305,14 @@ public class MappingBuilder implements Serializable {
                             if (!annotationList.isEmpty()) {
                                 //Get main Field (The First)
                                 Annotation mainAnnotation = annotationList.get(0);
-                                this.processElasticField(mainAnnotation, false);
+                                this.processElasticAnnotationField(mainAnnotation, false);
                                 annotationList.remove(mainAnnotation);
 
                                 //If contains more fields.
                                 if (!annotationList.isEmpty()) {
                                     this.mapping.startObject("fields");
                                     for (Annotation otherAnnotation : annotationList) {
-                                        this.processElasticField(otherAnnotation, true);
+                                        this.processElasticAnnotationField(otherAnnotation, true);
                                     }
                                     //fields
                                     this.mapping.endObject();
