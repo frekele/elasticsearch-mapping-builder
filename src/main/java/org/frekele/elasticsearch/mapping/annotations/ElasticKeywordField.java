f583d395c5c9770889c08df7d87a1dbcdd9fa090
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-17 01:52:20 -0300

    created ElasticKeywordField

---
+++ src/main/java/org/frekele/elasticsearch/mapping/annotations/ElasticKeywordField.java
@@ -0,0 +1,41 @@
+package org.frekele.elasticsearch.mapping.annotations;
+
+import org.frekele.elasticsearch.mapping.enums.FieldType;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * @see <a href="https://www.elastic.co/guide/en/elasticsearch/reference/current/keyword.html">elasticsearch keyword field</a>
+ */
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface ElasticKeywordField {
+
+    FieldType type = FieldType.KEYWORD;
+
+    String analyzer() default "";
+
+    float boost() default 1.0f;
+
+    boolean docValues() default false;
+
+    int ignoreAbove() default 256;
+
+    boolean index() default true;
+
+    String indexOptions() default "";
+
+    boolean norms() default true;
+
+    String nullValue() default "";
+
+    boolean store() default false;
+
+    String similarity() default "";
+
+    String normalizer() default "";
+
+}
