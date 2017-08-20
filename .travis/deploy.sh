fe9167c6ad023a3737c37ff187e6e28027c23a98
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-20 10:27:30 -0300

    update script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-cd `dirname $0`/.. 
+cd `dirname $0`/..
 
-if [ -z "$SONATYPE_USERNAME" ]
+if [ -z "${SONATYPE_USERNAME}" ]
 then
     echo "error: please set SONATYPE_USERNAME and SONATYPE_PASSWORD environment variable"
     exit 1
 fi
 
-if [ -z "$SONATYPE_PASSWORD" ]
+if [ -z "${SONATYPE_PASSWORD}" ]
 then
     echo "error: please set SONATYPE_PASSWORD environment variable"
     exit 1
 fi
 
-if [ ! -z "$TRAVIS_TAG" ]
+if [ ! -z "${TRAVIS_TAG}" ]
 then
-    echo "on a tag -> set pom.xml <version> to $TRAVIS_TAG"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.4:set -DnewVersion=$TRAVIS_TAG 1>/dev/null 2>/dev/null
+    echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
+    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.4:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
