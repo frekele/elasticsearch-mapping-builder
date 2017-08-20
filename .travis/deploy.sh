1720d4b0bdfca862acdcb00e82910d8f38584de3
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-20 12:28:56 -0300

    changed config to maven.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,9 +16,9 @@ fi
 if [ ! -z "${TRAVIS_TAG}" ]
 then
     echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
-    mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.4:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
+    ${MVN_HOME}/bin/mvn --settings .travis/settings.xml org.codehaus.mojo:versions-maven-plugin:2.4:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
 fi
 
-mvn clean deploy --settings .travis/settings.xml -DskipTests=true -B -U
+${MVN_HOME}/bin/mvn clean deploy --settings .travis/settings.xml -B -U
