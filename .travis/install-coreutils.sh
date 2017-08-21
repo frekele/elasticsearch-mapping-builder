eb0c5c69662e3ad40cc7fd7f76466afa2adea624
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-21 04:00:30 -0300

    Update install-coreutils.sh

--- .travis/install-coreutils.sh
+++ .travis/install-coreutils.sh
@@ -5,21 +5,21 @@ if [ ! -d "${HOME_CORE_UTILS}/bin" ]; then
    export FORCE_UNSAFE_CONFIGURE=1
    mkdir -p ${HOME_CORE_UTILS}
    
-   echo "DOWNLOAD coreutils-${CORE_UTILS}!"
+   echo "DOWNLOAD coreutils-${CORE_UTILS_VERSION}!"
    wget -qO- https://ftp.gnu.org/gnu/coreutils/coreutils-${CORE_UTILS_VERSION}.tar.xz -O /tmp/coreutils-${CORE_UTILS_VERSION}.tar.xz
    tar -xf /tmp/coreutils-${CORE_UTILS_VERSION}.tar.xz -C /tmp/
    chmod 777 -R /tmp/coreutils-${CORE_UTILS_VERSION}
    
    cd /tmp/coreutils-${CORE_UTILS_VERSION}
-   echo "CONFIGURE!"
+   echo "CONFIGURE coreutils-${CORE_UTILS_VERSION}!"
    ./configure --prefix=${HOME_CORE_UTILS}/ \
                --libexecdir=${HOME_CORE_UTILS}/lib \
                --enable-no-install-program=kill,uptime
 
-   echo "MAKE!"
+   echo "MAKE coreutils-${CORE_UTILS_VERSION}!"
    make
    
-   echo "MAKE INSTALL!"
+   echo "MAKE INSTALL coreutils-${CORE_UTILS_VERSION}!"
    make install
 fi
 
