f9b041e9676efb9336a14057dc65eb192fb47e96
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-21 03:04:09 -0300

    Create install-coreutils.sh

---
+++ .travis/install-coreutils.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CORE_UTILS=8.27
+export FORCE_UNSAFE_CONFIGURE=1
+
+
+wget -qO- https://ftp.gnu.org/gnu/coreutils/coreutils-${CORE_UTILS}.tar.xz -O /tmp/coreutils-${CORE_UTILS}.tar.xz
+tar -xf /tmp/coreutils-${CORE_UTILS}.tar.xz -C /tmp/
+chmod 777 -R /tmp/coreutils-${CORE_UTILS}
+
+/tmp/coreutils-${CORE_UTILS}/configure \
+                             --prefix=${HOME}/coreutils \
+                             --libexecdir=${HOME}/coreutils/lib \
+                             --enable-no-install-program=kill,uptime
+
+/tmp/coreutils-${CORE_UTILS}/make
+/tmp/coreutils-${CORE_UTILS}/make install
+
+${HOME}/coreutils/bin/base64 --version
