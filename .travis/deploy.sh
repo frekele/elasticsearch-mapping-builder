4345779799fd2e50ee9caf051c5f2f2d4c696c82
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-22 03:52:31 -0300

    .

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -16,7 +16,7 @@ fi
 if [ ! -z "${TRAVIS_TAG}" ]
 then
     echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
-    ${MVN_HOME}/bin/mvn --settings .travis/settings.xml versions:set -DnewVersion=${TRAVIS_TAG}
+    ${MVN_HOME}/bin/mvn --settings .travis/settings.xml versions:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
     #${MVN_HOME}/bin/mvn --settings .travis/settings.xml clean deploy -DskipTests=true -B -U
 else
     echo "not on a tag -> keep snapshot version in pom.xml"
