ad193fe90920ec300aa38a30ce7b0c29d73a180c
Leandro Kersting de Freitas <leandro.freitas@softdevelop.com.br>
2017-08-22 04:12:56 -0300

    configuration for travis-ci.

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,24 +1,36 @@
 #!/bin/bash
 cd `dirname $0`/..
 
+if [ -z "${GPG_PASSPHRASE}" ]
+then
+    echo "Error: Please set GPG_PASSPHRASE environment variable."
+    exit 1
+fi
+
 if [ -z "${SONATYPE_USERNAME}" ]
 then
-    echo "error: please set SONATYPE_USERNAME environment variable"
+    echo "Error: Please set SONATYPE_USERNAME environment variable."
     exit 1
 fi
 
 if [ -z "${SONATYPE_PASSWORD}" ]
 then
-    echo "error: please set SONATYPE_PASSWORD environment variable"
+    echo "Error: Please set SONATYPE_PASSWORD environment variable."
     exit 1
 fi
 
 if [ ! -z "${TRAVIS_TAG}" ]
 then
-    echo "on a tag -> set pom.xml <version> to ${TRAVIS_TAG}"
+    echo "TRAVIS_TAG=${TRAVIS_TAG}."
+    echo "Setting new version into pom.xml to ${TRAVIS_TAG}."
+    #To avoid Travis error, you need to redirect output to / dev / null
     ${MVN_HOME}/bin/mvn --settings .travis/settings.xml versions:set -DnewVersion=${TRAVIS_TAG} 1>/dev/null 2>/dev/null
-    #${MVN_HOME}/bin/mvn --settings .travis/settings.xml clean deploy -DskipTests=true -B -U
-else
-    echo "not on a tag -> keep snapshot version in pom.xml"
+    echo "DEPLOYING RELEASE!"
     ${MVN_HOME}/bin/mvn --settings .travis/settings.xml clean deploy -DskipTests=true -B -U
+    echo "DEPLOYED RELEASE!"
+else
+    echo "Snapshot version in pom.xml."
+    echo "DEPLOYING SNAPSHOT!"
+    ${MVN_HOME}/bin/mvn --settings .travis/settings.xml clean deploy -DskipTests=true -B -U
+    echo "DEPLOYED SNAPSHOT!"
 fi
